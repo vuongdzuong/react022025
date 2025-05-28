1a8415c2ee15d6bd23bb1e94c581991eb2a64220
vuongdzuong <vuongdzuong@gmail.com>
2025-05-28 21:31:59 +0700

    video 22

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,17 @@
 //Định nghĩa 1 component, thực chất là 1 hàm mũi tên arrow function
 //component = html + css + js
+
+import '../../../public/css/myStyle.css';
+
 const MyComponent = () =>{
   return (
-    <div>Bé Gảy su kem</div>
+    <>
+        <div className="mau_chu">Bé Gảy su kem</div>
+        <div style={{color:'red'}} >Bé Tin</div>
+        <div >Bé Bon</div>
+        <div >Bé Lu</div>
+    </>
+    
   );
 }
 
